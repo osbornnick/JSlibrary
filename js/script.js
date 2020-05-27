ff2eb5c935a4e50032a2f6e51f327b1fec7bdf9d
Nicholas Osborn <nicholas.p.osborn@gmail.com>
2020-05-27 13:21:43 -0400

    render() implemented

--- js/script.js
+++ js/script.js
@@ -13,15 +13,35 @@ Book.prototype.info = function() {
 }
 
 function addBookToLibrary() {
-
+  const title = prompt('Title: ');
+  const author = prompt('Author: ');
+  const pages = prompt('Number of pages: ');
+  const readString = prompt('Have you read it? (true/false): ')
+  if (readString == 'true') {
+    const read = true;
+  } else {
+    const read = false;
+  }
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
 }
 
 function render() {
-  myLibrary.forEach(ele => {
-
+  const container = document.querySelector('.container');
+  myLibrary.forEach(book => {
+    const row = document.createElement('div');
+    row.className = 'row';
+    const card = document.createElement('div');
+    card.className = 'card'
+    card.textContent = book.info();
+    row.appendChild(card);
+    container.appendChild(row);
   })
 }
 
 const book = new Book('The Hobbit', 'J.R.R. Tolkien', '295', true);
-console.log(book.info());
-console.log(book.constructor);
+myLibrary.push(book);
+render();
+
+// console.log(book.info());
+// console.log(book.constructor);
